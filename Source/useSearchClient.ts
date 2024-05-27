e684d6844e15197c02503c7fb78b863879c8d73d
Nikola Hristov <Nikola@PlayForm.Cloud>
2024-05-27 19:34:40 +0300

    squash!

--- Source/useSearchClient.ts
+++ Source/useSearchClient.ts
@@ -1,24 +1,24 @@
 import { MeiliSearch } from "meilisearch";
-import { type Accessor, createMemo } from "solid-js";
+import { Accessor, createMemo } from "solid-js";
 import version from "./version";
 
 export function useSearchClient({
-	host,
-	apiKey,
-	clientAgents = [],
+  host,
+  apiKey,
+  clientAgents = [],
 }: {
-	host: string;
-	apiKey: string;
-	clientAgents?: string[];
+  host: string;
+  apiKey: string;
+  clientAgents?: string[];
 }): Accessor<MeiliSearch> {
-	return createMemo(
-		() =>
-			new MeiliSearch({
-				host,
-				apiKey,
-				clientAgents: clientAgents.concat(
-					`Meilisearch docs-searchbar.js (v${version}`,
-				),
-			}),
-	);
+  return createMemo(
+    () =>
+      new MeiliSearch({
+        host,
+        apiKey,
+        clientAgents: clientAgents.concat(
+          `Meilisearch docs-searchbar.js (v${version}`,
+        ),
+      }),
+  );
 }
--- example/Source/main.ts
+++ example/Source/main.ts
@@ -2,8 +2,8 @@ import { docsearch } from "meilisearch-docsearch";
 import "meilisearch-docsearch/css";
 
 docsearch({
-	host: "https://ms-4ebb96f179f0-1619.fra.meilisearch.io",
-	apiKey: "3eb6db150af1abefe000f00386e149dfb5a006932cab55d1ccd810b8672a4e12",
-	indexUid: "docs-v1",
-	container: "#docsearch",
+  host: "https://ms-4ebb96f179f0-1619.fra.meilisearch.io",
+  apiKey: "3eb6db150af1abefe000f00386e149dfb5a006932cab55d1ccd810b8672a4e12",
+  indexUid: "docs-v1",
+  container: "#docsearch",
 });
